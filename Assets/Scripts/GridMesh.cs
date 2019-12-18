8b6674b236c902c7ff21696ea937fa066bf59bef
menegais1 <menegais1@gmail.com>
2019-12-18 12:26:08 -0300

    Started working on implementing the vertex clustering simplification algorithm

--- Assets/Scripts/GridMesh.cs
+++ Assets/Scripts/GridMesh.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,6 +12,11 @@ public class GridMesh : MonoBehaviour
     public float precision;
     public float scale;
     public RawImage texture;
+
+    //Grid debug
+    public int gridSubdivisions;
+
+
     private int col;
     private int lin;
     private Vector3[,] meshVertices;
@@ -20,7 +26,7 @@ public class GridMesh : MonoBehaviour
     {
         col = Mathf.CeilToInt((maxBound.x - minBound.x) / precision) + 1;
         lin = Mathf.CeilToInt((maxBound.y - minBound.y) / precision) + 1;
-        texture.texture = NoiseGenerator.GenerateTexture(1920, 1080, minBound, precision, 50);
+        //  texture.texture = NoiseGenerator.GenerateTexture(1920, 1080, minBound, precision, 50);
     }
 
     void Update()
@@ -56,6 +62,14 @@ public class GridMesh : MonoBehaviour
                 {
                     Gizmos.DrawSphere(meshVertices[i, j], 0.1f);
                 }
+            }
+        }
+
+        if (grid != null)
+        {
+            for (var i = 0; i < grid.Count; i++)
+            {
+                Gizmos.DrawWireCube(grid[i].bounds.center, grid[i].bounds.size);
             }
         }
     }
@@ -97,5 +111,105 @@ public class GridMesh : MonoBehaviour
         m.RecalculateNormals();
 
         meshFilter.mesh = m;
+
+        VertexClusteringMeshSimplification(m, gridSubdivisions);
+    }
+
+
+    public class Vertex
+    {
+        public Vector3 vertex;
+        public List<Vertex> adjacent;
+        public int cluster;
+
+        public Vertex()
+        {
+            adjacent = new List<Vertex>();
+            cluster = -1;
+        }
+
+        public static void AddAdjacent(Vertex v1, Vertex v2)
+        {
+            v1.adjacent.Add(v2);
+            v2.adjacent.Add(v1);
+        }
+    }
+
+    public class AdjacencyMatrix
+    {
+        public List<Vertex> vertices;
+        public int vertexNumber;
+        public Bounds bounds;
+
+        public AdjacencyMatrix(Mesh mesh)
+        {
+            vertexNumber = mesh.vertices.Length;
+            Vector3[] vertices = mesh.vertices;
+            int[] triangles = mesh.triangles;
+            bounds = mesh.bounds;
+
+            this.vertices = new List<Vertex>(vertexNumber);
+            for (int i = 0; i < vertexNumber; i++)
+            {
+                this.vertices.Add(new Vertex {vertex = vertices[i]});
+            }
+
+            for (int i = 0; i < triangles.Length - 3; i += 3)
+            {
+                Vertex v1 = this.vertices[triangles[i + 0]];
+                Vertex v2 = this.vertices[triangles[i + 1]];
+                Vertex v3 = this.vertices[triangles[i + 2]];
+                Vertex.AddAdjacent(v1, v2);
+                Vertex.AddAdjacent(v1, v3);
+                Vertex.AddAdjacent(v2, v3);
+            }
+        }
+    }
+
+    public class GridCell
+    {
+        public Bounds bounds;
+        public int cluster;
+        public Vertex representative;
+        public List<Vertex> clusterized;
+
+        public GridCell(Bounds bounds, int cluster)
+        {
+            clusterized = new List<Vertex>();
+            this.bounds = bounds;
+            this.cluster = cluster;
+        }
+    }
+
+
+    public List<GridCell> grid;
+
+    public void VertexClusteringMeshSimplification(Mesh mesh, int gridSubdivisions)
+    {
+        AdjacencyMatrix matrix = new AdjacencyMatrix(mesh);
+
+        grid = new List<GridCell>(gridSubdivisions);
+        Vector3 gridCellExtent = matrix.bounds.size / gridSubdivisions;
+        for (int i = 0; i < gridSubdivisions; i++)
+        for (int j = 0; j < gridSubdivisions; j++)
+        for (int k = 0; k < gridSubdivisions; k++)
+            grid.Add(new GridCell(
+                new Bounds(
+                    matrix.bounds.min + new Vector3(gridCellExtent.x * i, gridCellExtent.y * j, gridCellExtent.z * k) +
+                    gridCellExtent / 2,
+                    gridCellExtent), k));
+
+
+        for (var i = 0; i < grid.Count; i++)
+        {
+            for (var i1 = 0; i1 < matrix.vertices.Count; i1++)
+            {
+                if (grid[i].bounds.Contains(matrix.vertices[i].vertex))
+                {
+                    matrix.vertices[i].cluster = grid[i].cluster;
+                    grid[i].clusterized.Add(matrix.vertices[i]);
+                }
+            }
+        }
     }
 }
